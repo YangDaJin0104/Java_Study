fd82b9d67c00c88a4d8122923bf68e6dc48ca144
dajinYang <didekwls0104@naver.com>
2024-07-02 19:16:02 +0900

    문자열 결합

--- Chapter1~3/JavaStudy_Chapter/src/Main.java
+++ Chapter1~3/JavaStudy_Chapter/src/Main.java
@@ -1,13 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 100;
-        double pi = 3.14;
-        char ch = 'a';
-        String str = "ABC";
+        String name = "Java" + "_" + "Study"; //Java_Study
 
-        System.out.println(x); // 정수(integer)를 저장할 변수의 타입은 int로
-        System.out.println(pi); // 실수를 저장할 변수의 타입은 double로
-        System.out.println(ch); // 문자(1개)를 저장할 변수의 타입은 char로
-        System.out.println(str); // 여러 문자(0~n개)를 저장할 변수의 타입은 String로
+        System.out.println(name);//Java_Study
     }
 }
