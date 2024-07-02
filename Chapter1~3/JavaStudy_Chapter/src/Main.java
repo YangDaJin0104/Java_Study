6ef42895a7afcc6a16092874c015ec0dd2ad5ff7
dajinYang <didekwls0104@naver.com>
2024-07-02 19:01:36 +0900

    변수의 타입

---
+++ Chapter1~3/JavaStudy_Chapter/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 100;
+        double pi = 3.14;
+        char ch = 'a';
+        String str = "ABC";
+
+        System.out.println(x); // 정수(integer)를 저장할 변수의 타입은 int로
+        System.out.println(pi); // 실수를 저장할 변수의 타입은 double로
+        System.out.println(ch); // 문자(1개)를 저장할 변수의 타입은 char로
+        System.out.println(str); // 여러 문자(0~n개)를 저장할 변수의 타입은 String로
+    }
+}
