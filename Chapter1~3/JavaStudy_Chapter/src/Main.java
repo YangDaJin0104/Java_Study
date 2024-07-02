d039f14f8341ba70c4a4d6890f7b0b1d8efe7c02
dajinYang <didekwls0104@naver.com>
2024-07-02 22:42:17 +0900

    지시자

--- Chapter1~3/JavaStudy_Chapter/src/Main.java
+++ Chapter1~3/JavaStudy_Chapter/src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        String name = "Java" + "_" + "Study"; //Java_Study
+        int i = 10;
+        float f = 3.14f;
+        double d = 1.23456789;
+        char c = 'A';
+        String s = "ABC";
 
-        System.out.println(name);//Java_Study
+        System.out.printf("i = %d%n", i); // %n = 줄바꿈
+        System.out.printf("f = %f%n", f);
+        System.out.printf("d = %f%n", d); // 마지막 자리 반올림됨
+        System.out.printf("c = %c%n", c);
+        System.out.printf("s = %s%n", s);
     }
 }
