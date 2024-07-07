6bab878054732570fa0be7d041140c8c0f37eb6f
dajinYang <didekwls0104@naver.com>
2024-07-07 16:51:15 +0900

    Chapter5 배열 파일 생성

---
+++ Chapter5 배열/JavaStudy_Chapter5/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int score[];
+        score = new int[10];
+
+        for (int i = 0; i < score.length; i++) {
+            score[i] = i * 10 + 100;
+        }
+    }
+}
